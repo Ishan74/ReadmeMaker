14b0269efbb6eca61dcf3ec6df83b200d8b8a8c9
ichaudhary <ishan.chaudhary@student.unimelb.edu.au>
2024-10-03 23:40:30 +1000

    modified:   index.js 	new file:   package-lock.json 	modified:   package.json

--- index.js
+++ index.js
@@ -1,13 +1,74 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+      type: 'input',
+      name: 'title',
+      message: 'What is the title of your project?',
+    },
+    {
+      type: 'input',
+      name: 'description',
+      message: 'Provide a description of your project:',
+    },
+    {
+      type: 'input',
+      name: 'installation',
+      message: 'What are the installation instructions?',
+    },
+    {
+      type: 'input',
+      name: 'usage',
+      message: 'What is the usage information?',
+    },
+    {
+      type: 'input',
+      name: 'contributing',
+      message: 'What are the contribution guidelines?',
+    },
+    {
+      type: 'input',
+      name: 'tests',
+      message: 'What are the test instructions?',
+    },
+    {
+      type: 'list',
+      name: 'license',
+      message: 'Choose a license for your project:',
+      choices: ['MIT', 'Apache 2.0', 'GPLv3', 'None'],
+    },
+    {
+      type: 'input',
+      name: 'github',
+      message: 'Enter your GitHub username:',
+    },
+    {
+      type: 'input',
+      name: 'email',
+      message: 'Enter your email address:',
+    }
+  ];
+  
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
-
-// TODO: Create a function to initialize app
-function init() {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) =>
+      err ? console.error(err) : console.log('README.md generated successfully!')
+    );
+  }
 
 // Function call to initialize app
-init();
+function init() {
+    inquirer.prompt(questions)
+      .then((answers) => {
+        const markdownContent = generateMarkdown(answers);
+        writeToFile('README.md', markdownContent);
+      })
+      .catch((error) => console.error(error));
+  }
+  
+  // Function call to initialize app
+  init();
